4531bcfdc0709a0b90fca563c572cc668b3edb54
tanglongan <tanglongan@sogou.com>
2021-10-28 15:32:01 +0800

    组合模式

---
+++ src/test/java/com/tanglongan/dp/Dp_08_test.java
@@ -0,0 +1,19 @@
+package com.tanglongan.dp;
+
+import com.tanglongan.dp08_combination.EngineController;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Dp_08_test {
+
+    private Logger logger = LoggerFactory.getLogger(Dp_08_test.class);
+
+    @Test
+    public void test_EngineController() {
+        EngineController engineController = new EngineController();
+        String process = engineController.process("Oli09pLkdjh", "man", 29);
+        logger.info("测试结果：{}", process);
+    }
+
+}
